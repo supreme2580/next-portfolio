14afecf26e041bf9ef494a41dffa7c10ad45f330
Jose Luis Bautista <jose.luis.bautista@obf.ateneo.edu>
2023-10-19 17:21:06 +0800

    feat: added about section and images

---
+++ app/components/HomeSection.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import profile from "@/public/profilepic.png";
+
+type HomeProps = {
+  views: number;
+};
+
+export const HomeSection = ({ views }: HomeProps) => {
+  return (
+    <section
+      className="min-h-screen flex flex-col justify-center align-middle"
+      id="home"
+    >
+      <Image
+        src={profile}
+        alt="profile"
+        width={200}
+        height={200}
+        className="self-center rounded-full"
+      />
+      <div className="pb-4 pt-8 sm:pt-4 text-3xl">hi, I'm Luis 👋</div>
+      <div className="text-justify text-lg tracking-tight mb-12">
+        I am a fresh graduate from{" "}
+        <span className="text-[#031090]">the Ateneo de Manila University</span>{" "}
+        and I majored in Computer Science. Currently I am a L0.5 Software
+        Engineer a.k.a <b>Aspiring Software Engineer</b>.
+      </div>
+      {/* <div>Website Visitors: {views}</div> */}
+    </section>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,22 @@
 import { ReportView } from "./components/ReportView";
 import { Redis } from "@upstash/redis";
+import { HomeSection } from "./components/HomeSection";
+import { AboutSection } from "./components/AboutSection";
 
 export const revalidate = 60;
 
 const redis = Redis.fromEnv();
 
 export default async function Home() {
-  const views = (await redis.get<number>(["profileViews"].join(":"))) ?? 0;
+  // const views = (await redis.get<number>(["profileViews"].join(":"))) ?? 0;
+  const views = 0;
   return (
-    <div className="antialiased max-w-2xl mb-40 flex flex-col mx-4 md:mx-auto">
-      <ReportView />
+    <div className="antialiased max-w-2xl mb-40 flex flex-col mx-14 md:mx-auto">
+      {/* <ReportView /> */}
+
       <main className="flex-auto min-w-0 flex flex-col px-2 sm:px-0">
-        <section
-          className="min-h-screen flex flex-col justify-center align-middle"
-          id="home"
-        >
-          <div className="pb-4 text-3xl">hi, I'm Luis 👋</div>
-          <div className="text-justify text-lg tracking-tight">
-            I am a fresh graduate from{" "}
-            <span className="text-[#031090]">
-              the Ateneo de Manila University
-            </span>{" "}
-            and I majored in Computer Science. Currently I am a L0.5 Software
-            Engineer a.k.a Aspiring Software Engineer.
-          </div>
-          <div>Website Visitors: {views}</div>
-        </section>
-        <h1 className="text-3xl min-h-screen" id="about">
-          About
-        </h1>
+        <HomeSection views={views} />
+        <AboutSection />
         <h1 className="text-3xl min-h-screen" id="work">
           Work Experience
         </h1>
